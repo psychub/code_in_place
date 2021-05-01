4821d82c6ae0d90d2cb1f772a7e80fd76187c107
psychub <krause.linus@googlemail.com>
2021-05-01 08:47:06 +0200

    trying return statement

--- python_1.py
+++ python_1.py
@@ -3,8 +3,16 @@ import math
 
 
 def main():
-    print("hello world")
-    print(math.pi * 4)
+    print(test([1, 2, 3, 4, 5, 6]))
+
+
+def test(numbers):
+    return [num for num in numbers if not num % 2]
+
+
+
+
+
 
 
 if __name__=="__main__":
