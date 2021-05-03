a7b547dc2144e85948f1854eb464925d63fdacab
psychub <krause.linus@googlemail.com>
2021-05-03 12:33:11 +0200

    using the global statement

--- python_1.py
+++ python_1.py
@@ -1,25 +1,13 @@
-import math
-
-CONSTANT = 6.56784
-
 
 
 def main():
-    print(test([1, 2, 3, 4, 5, 6]))
     getting_name()
-    username * 2
-
-
-
-def test(numbers):
-    return [num for num in numbers if not num % 2]
-
+    print("Hello " + username)
 
 
 def getting_name():
-    return username = input("please enter your name: ")
-
-
+    global username
+    username = input("please enter your name ")
 
 
 if __name__=="__main__":
